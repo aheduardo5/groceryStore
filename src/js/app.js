099c98ba3d2deb282a977bf12facb3c2d1c2a985
Eduardo Aguilar <aheduardo5@gmail.com>
2021-10-17 20:16:49 -0500

    home.scss: terminado

--- src/js/app.js
+++ src/js/app.js
@@ -43,3 +43,19 @@ window.onscroll = () => {
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+// SLIDES IMAGES
+let slides = document.querySelectorAll(".home .slides-container .slide");
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
